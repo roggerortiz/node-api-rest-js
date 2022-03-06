37c6d8a14c12cc400c4097187d80ad7d61e29627
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2022-03-06 14:27:30 -0500

    fix file json for swagger specs

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import swaggerUI from 'swagger-ui-express';
-import swaggerDocs from '../swagger';
+import swaggerSpecs from '../../swagger.json';
 import cors from '../middlewares/cors';
 import jwt from '../middlewares/jwt';
 import authRouter from '../routes/auth.routes';
@@ -22,7 +22,7 @@ app.use(cors);
 app.use('/', homeRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
 const startServer = () => {
    app.listen({ port: process.env.PORT }, () => {
--- src/swagger/index.js
+++ src/swagger/index.js
@@ -1,33 +1,47 @@
+import fs from 'fs';
+import path from 'path';
 import getTags from "./tags";
 import getPaths from "./paths";
 import getComponents from "./components";
 
 const models = ['user'];
 
-const swaggerDocs = {
-   openapi: '3.0.0',
-   info: {
-      title: "Rest API",
-      version: "1.0.0",
-   },
-   host: "localhost:4000",
-   basePath: "/",
-   consumes: ['application/json'],
-   produces: ['application/json'],
-   tags: getTags(models),
-   components: getComponents(),
-   security: [
-      { bearerAuth: [] }
-   ],
-   securityDefinitions: {
-      apiKeyAuth: {
-         type: "http",
-         in: "header",
-         name: "Authorization",
-         description: "Json Web Token"
-      }
-   },
-   paths: getPaths(models)
-};
+const getSwaggerSpecs = () => {
+   const tags = getTags(models);
+   const paths = getPaths(models)
+   const components = getComponents(models);
 
-export default swaggerDocs;
+   return {
+      openapi: '3.0.0',
+      info: {
+         title: "Rest API",
+         version: "1.0.0",
+      },
+      host: "localhost:4000",
+      basePath: "/",
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags,
+      components,
+      security: [
+         { bearerAuth: [] }
+      ],
+      securityDefinitions: {
+         apiKeyAuth: {
+            type: "http",
+            in: "header",
+            name: "Authorization",
+            description: "Json Web Token"
+         }
+      },
+      paths
+   };
+}
+
+const generateDocs = () => {
+   const swaggerSpecs = getSwaggerSpecs();
+   const filePath = path.join(__dirname, './..', './..', 'swagger.json');
+   fs.writeFileSync(filePath, JSON.stringify(swaggerSpecs, null, 3));
+}
+
+generateDocs();
