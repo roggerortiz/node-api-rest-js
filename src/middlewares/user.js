939129924fe46028bc6432cc11700536401d39ab
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2022-03-05 21:25:01 -0500

    fix user middleware

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -3,14 +3,16 @@ import User from "../models/User";
 const validateUser = async (req, res, next) => {
    try {
 
+      const userId = req.params.id;
       const { username, email } = req.body;
 
       const user = await User.findOne().or([{ username }, { email }]);
+      const userIdBD = user._id.toString()
 
-      if (user && user.username === username)
+      if (user && userIdBD !== userId && user.username === username)
          return res.status(400).json({ message: "Username alredy exists" });
 
-      if (user && user.email === email)
+      if (user && userIdBD !== userId && user.email === email)
          return res.status(400).json({ message: "Email alredy exists" });
 
       return next();
